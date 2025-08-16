d67872600e9709782e04a07d6bc51b5cf0bc3cc4
brendhacasaro <brendhameira9@gmail.com>
2025-08-16 10:41:59 -0300

    .

--- transaction/transaction_store.go
+++ transaction/transaction_store.go
@@ -2,6 +2,7 @@ package transaction
 
 import (
 	"fmt"
+	"github.com/google/uuid"
 )
 
 type TransactionStore struct {
@@ -11,4 +12,16 @@ type TransactionStore struct {
 func (ts *TransactionStore) Insert(transaction Transaction) {
 	ts.store = append(ts.store, transaction)
 	fmt.Printf("%+v\n", ts.store)
+}
+
+func (ts *TransactionStore) SearchById(id uuid.UUID) Transaction {
+	for _, transaction := range ts.store {
+		if transaction.id == id {
+			// fmt.Printf("%T\n", transaction.id)
+			// fmt.Printf("%T\n", id)
+			return transaction
+		}
+	}
+
+	return Transaction{}
 }
--- transaction/transaction_store_test.go
+++ transaction/transaction_store_test.go
@@ -10,6 +10,20 @@ func TestInsert(t *testing.T) {
 	ts.Insert(NewTransaction("teste", "teste", 100, time.Now()))
 
 	if ts.store == nil {
-		t.Fatalf("The fields of transaction don't should not be nil")
+		t.Fatalf("The fields of transaction should not be nil")
+	}
+}
+
+func TestSearchById(t *testing.T) {
+	ts := TransactionStore{}
+
+	ts.Insert(NewTransaction("teste", "teste", 100, time.Now()))
+	ts.Insert(NewTransaction("teste2", "teste2", 200, time.Now()))
+
+	transactionSearch := ts.store[1].id
+	transactionFound := ts.SearchById(transactionSearch)
+
+	if transactionFound.id != transactionSearch {
+		t.Fatalf("The ID founded is different of ID/transaction wanted")
 	}
 }
